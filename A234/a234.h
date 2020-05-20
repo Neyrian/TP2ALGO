3779bfe0c0f4d6eed93b63c4a0ca7a113039a95e
Jules Gonzalez <julesgonzalez99@yahoo.fr>
2020-05-20 14:53:05 +0200

    a234.h updated

--- A234/a234.h
+++ A234/a234.h
@@ -4,11 +4,11 @@
 
 
 typedef struct n {
-  
+
   int      t         ; /* t type de noeud 0, 2, 3 ou 4 */
   int      cles  [3] ;
   struct n *fils [4] ;
-  
+
 } noeud234, *pnoeud234 ;
 
 /* type Arbre, pointeur vers un noeud */
@@ -21,4 +21,6 @@ void afficher_arbre (Arbre234 a, int niveau) ;
 
 void ajouter_cle (Arbre234 *a, int cle, int niveau, Arbre234 pere) ;
 
-  
+int NombreCles (Arbre234 a);
+
+Arbre234 RechercherCle (Arbre234 a, int cle);
