57e466c7509cf98c87bea3aeb83e5d6b9797c8be
Alexis Lanquetin <alexis.lanquetin@etu.univ-grenoble-alpes.fr>
2020-05-20 15:14:06 +0200

    ajout de test min max

--- A234/test234.c
+++ A234/test234.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include "a234.h"
 
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
 
 int nbclef(char *nom_fichier) {
   FILE *f ;
@@ -16,6 +18,31 @@ int nbclef(char *nom_fichier) {
   return nb ;
 }
 
+int clefmax(char *nom_fichier) {
+  FILE *f ;
+  int cle;
+  int clef_max = -1;
+  f = fopen (nom_fichier, "r") ;
+  while (fscanf (f, "%d", &cle) != EOF) {
+    clef_max = max(cle, clef_max);
+  }
+  fclose (f) ;
+  return clef_max ;
+}
+
+int clefmin(char *nom_fichier) {
+  FILE *f ;
+  int cle;
+  f = fopen (nom_fichier, "r") ;
+  fscanf (f, "%d", &cle);
+  int clef_min = cle;
+  while (fscanf (f, "%d", &cle) != EOF) {
+    clef_min = max(cle, clef_min);
+  }
+  fclose (f) ;
+  return clef_min;
+}
+
 int main(int argc, char** argv) {
   if (argc != 2) {
     fprintf (stderr, "il manque le parametre nom de fichier\n") ;
@@ -26,6 +53,8 @@ int main(int argc, char** argv) {
   Arbre234 arbre_test = lire_arbre (argv[1]) ;
   printf ("==== Afficher arbre ====\n") ;
   afficher_arbre (arbre_test, 0) ;
-  printf("Test de la fonction nombre de clef\nResultat : %d, Resultat attentu : %d \n", NombreCles(arbre_test), nbclef(argv[1]));
+  printf("Test de la fonction nombre de clef\nResultat : %d, Resultat attendu : %d \n", NombreCles(arbre_test), nbclef(argv[1]));
+  printf("Test de la fonction clef min\nResultat : %d, Resultat attendu : %d\n", CleMin(arbre_test), clefmin(argv[1]));
+  printf("Test de la fonction clef max\nResultat : %d, Resultat attendu : %d\n", CleMax(arbre_test), clefmax(argv[1]));
   return 0;
 }
